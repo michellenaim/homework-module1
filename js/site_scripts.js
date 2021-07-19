f48bbf7d67ec9925748ec40e1e0ef9b3c5d2fa95
Michelle Naim <michellenaim@gmail.com>
2021-07-18 21:39:00 -0700

    Refactor old code and reorganize

---
+++ js/site_scripts.js
@@ -0,0 +1,17 @@
+var photos = [];
+var fileNames = [];
+var imageList = [];
+var image = [];
+var openList = "<li class='partner'>";
+var closeList = "</li>";
+var typesOfPartners = ["bustour", "cabinrental", "campingadv", "collegetours", "rentalbike", "tourgroup"]
+
+for(var i=0; i<6; i++){
+  fileNames.push('partner-'+ typesOfPartners[i]);
+  console.log(fileNames)
+  photos.push("<img src='images/partners/"+ fileNames[i]+".png'>");
+  image = openList + photos[i] + closeList;
+  imageList.push(image)
+}
+
+document.getElementById('partners').innerHTML = imageList.join("")
